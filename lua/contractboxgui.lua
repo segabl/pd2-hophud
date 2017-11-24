96bf1d31685a66d63f159d9c2fd56ab8fd23becb
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-11-24 22:54:27 +0100

    Fix white names

--- lua/contractboxgui.lua
+++ lua/contractboxgui.lua
@@ -2,8 +2,8 @@ local create_character_text_original = ContractBoxGui.create_character_text
 function ContractBoxGui:create_character_text(peer_id, x, y, text, icon)
   create_character_text_original(self, peer_id, x, y, text, icon)
   
-  local name, level, rank, _ = HopHUD:information_by_peer(managers.network:session():peer(peer_id))
-  HopHUD:set_name_panel_text(self._peers[peer_id], name, level, rank)
+  local name, level, rank, color_id = HopHUD:information_by_peer(managers.network:session():peer(peer_id))
+  HopHUD:set_name_panel_text(self._peers[peer_id], name, level, rank, color_id)
 
   local _, _, w, h = self._peers[peer_id]:text_rect()
   self._peers[peer_id]:set_size(w, h)
