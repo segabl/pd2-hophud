29f9df21a49a936812c98e775f662b5dd94b3b3b
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-12-16 00:43:16 +0100

    Add invulnerability indicator for Copycat

--- lua/playermanager.lua
+++ lua/playermanager.lua
@@ -3,14 +3,14 @@ if not HopHUD.settings.display_invulnerability then
 end
 
 Hooks:PreHook(PlayerManager, "activate_temporary_upgrade", "activate_temporary_upgrade_hophud", function (self, category, upgrade)
-	if upgrade == "armor_break_invulnerable" then
+	if upgrade == "armor_break_invulnerable" or upgrade == "mrwi_health_invulnerable" then
 		local upgrade_value = self:upgrade_value(category, upgrade)
 		if upgrade_value == 0 then
 			return
 		end
 		local teammate_panel = managers.hud:get_teammate_panel_by_peer()
 		if teammate_panel then
-			teammate_panel:animate_invulnerability(upgrade_value[1])
+			teammate_panel:animate_invulnerability(upgrade_value[upgrade == "mrwi_health_invulnerable" and 2 or 1])
 		end
 	end
 end)
