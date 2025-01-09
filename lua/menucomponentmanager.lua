20ca424d3181f43f96dd90b6fcae815cc11bab7c
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-01-09 18:35:20 +0100

    Fully remove content updates panel with setting on
    This makes it so it is no longer selectable through keyboard or controller

--- lua/menucomponentmanager.lua
+++ lua/menucomponentmanager.lua
@@ -1,3 +1,9 @@
+if HopHUD.settings.hide_content_updates then
+	function MenuComponentManager:create_new_heists_gui()
+		self:close_new_heists_gui()
+	end
+end
+
 if HopHUD.settings.hide_news_feed then
 	function MenuComponentManager:create_newsfeed_gui()
 		self:close_newsfeed_gui()
--- lua/newheistsgui.lua
+++
@@ -1,5 +0,0 @@
-if HopHUD.settings.hide_content_updates then
-	function NewHeistsGui:set_enabled()
-		self._content_panel:set_visible(false)
-	end
-end
